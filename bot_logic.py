1035fdbb8795aae0b2dda7f6d6aa0fa796b5ed47
seekingARGA <mangoeater352@gmail.com>
2025-07-25 18:58:57 +0700

    Create bot_logic.py

---
+++ bot_logic.py
@@ -0,0 +1,27 @@
+import random
+
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "heads!!!"
+    else:
+        return "tails!!!"
+    
+def roll_dice():
+    return "the dice is", random.randint(1,6)
